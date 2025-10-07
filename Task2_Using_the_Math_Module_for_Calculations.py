e9301911b2ce20e8bcea87c29b1b16f0e23d2df6
Naseem Ahamad <137470997+Naseem030583@users.noreply.github.com>
2025-10-07 08:09:36 -0500

    Update Task2_Using_the_Math_Module_for_Calculations.py

--- Task2_Using_the_Math_Module_for_Calculations.py
+++ Task2_Using_the_Math_Module_for_Calculations.py
@@ -2,11 +2,9 @@ import math
 
 number = float(input("Enter a number: "))
 
-
 square_root = math.sqrt(number)
 natural_log = math.log(number)
 sine_value = math.sin(number)
-
 
 print(f"Square root: {square_root}")
 print(f"Logarithm: {natural_log}")
